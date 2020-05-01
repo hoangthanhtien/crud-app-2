6a0d2ba98c4aefd526e39f7baf523bdbdbc99eb2
hoangthanhtien <hoangtiendasilva@gmail.com>
2020-05-01 16:34:19 +0700

    Add files

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mybrary", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to the database!"));
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`The server is running at port ${PORT}`);
+});
